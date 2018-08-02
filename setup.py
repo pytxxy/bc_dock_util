27ce17cbb941f0de004cfe58057fdb30bbbb3407
caifh <cfh0081@163.com>
2018-08-02 17:41:05 +0800

    1.根据官方文档完善配置信息；

--- setup.py
+++ setup.py
@@ -10,33 +10,19 @@ setup(
     version='0.0.1',
     author='caifh',
     author_email='caifh@tianxiaxinyong.com',
-    description=(
-        'B client docking tianxiaxinyong service sdk util.'
-    ),
+    description='B client docking tianxiaxinyong service sdk util.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    maintainer='caifh',
-    maintainer_email='caifh@tianxiaxinyong.com',
-    license='BSD License',
     packages=find_packages(),
-    platforms=['all'],
-    url='https://www.tianxiaxinyong.com/',
+    url='https://github.com/pytxxy/bc_dock_util',
     install_requires=[
         'pycryptodome>=3.5.1',
         'cryptography>=2.3',
         'Requests>=2.19.1'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ]
 )

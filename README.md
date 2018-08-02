5bfc9a3ce5eaac56a73f739ed3d2de7ff97bed3d
caifh <cfh0081@163.com>
2018-08-02 17:54:25 +0800

    1.增加版本说明及增加版本号；

---
+++ README.md
@@ -0,0 +1,3 @@
+# B client docking tianxiaxinyong sdk util
+
+B端对接天下信用sdk工具模块，提供了RSA密钥生成方法、RSA加解密、AES加解密，以及JAVA使用的PKCS8私钥转换成PKCS1私钥等功能。
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='bc_dock_util',
-    version='0.0.1',
+    version='0.0.6',
     author='caifh',
     author_email='caifh@tianxiaxinyong.com',
     description='B client docking tianxiaxinyong service sdk util.',
